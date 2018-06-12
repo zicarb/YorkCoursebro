c299f2c6ca29f861942f3dc5dc123c54bae85e70
zicarb <zicarb09@gmail.com>
2018-06-12 01:05:15 -0400

    成功实现单次成功登陆和选课

--- shuake/script1.py
+++ shuake/script1.py
@@ -14,10 +14,10 @@ class CourseSelect(unittest.TestCase):
 
     def testLogin(self):
         driver = self.driver
-        PassportUsername = "zhx1997"
-        PassportPassword = "zyx2018"
-        term = int(1)
-        wait = WebDriverWait(driver,20)
+        PassportUsername = ""
+        PassportPassword = ""
+        wait = WebDriverWait(driver,10)
+        catnumber = ""
 
         accountNameId    = "mli"
         passFieldId      = "password"
@@ -34,10 +34,43 @@ class CourseSelect(unittest.TestCase):
         passFieldIdElement.send_keys(PassportPassword)
         time.sleep(3)
         LoginButtonElement.click()
-        driver=driver.current_window_handle
-        driver
+
+        enrollPageXpath = "/html/body/form/div[1]/table/tbody/tr[2]/td[2]/span/b"
+        wait.until(lambda driver: driver.find_element_by_xpath(enrollPageXpath))
+
+        submitButtonXpath = "/html/body/form/div[1]/table/tbody/tr[4]/td[2]/table/tbody/tr/td/table/tbody/tr[3]/td[2]/input"
+
+        select = Select(driver.find_element_by_xpath(
+            '/html/body/form/div[1]/table/tbody/tr[4]/td[2]/table/tbody/tr/td/table/tbody/tr[2]/td[2]/span/select'))
+        select.select_by_visible_text('FALL/WINTER 2018-2019 UNDERGRADUATE STUDENTS')
+        driver.find_element_by_xpath(submitButtonXpath).click()
+
+        addCourseXpath ="/html/body/form/div[1]/table/tbody/tr[4]/td[2]/table/tbody/tr/td/table[4]/tbody/tr[1]/td[1]/div/input"
+        addCourseElement = driver.find_element_by_xpath(addCourseXpath)
+        addCourseElement.click()
+
+        courseCatXpath ="/html/body/form/div[1]/table/tbody/tr[4]/td[2]/table/tbody/tr/td/span[2]/input[1]"
+        courseCatElment = wait.until(lambda driver: driver.find_element_by_xpath(courseCatXpath))
+        confirmXpath = "/html/body/form/div[1]/table/tbody/tr[4]/td[2]/table/tbody/tr/td/span[2]/input[2]"
+        confirmElement = wait.until(lambda driver: driver.find_element_by_xpath(confirmXpath))
+
+        courseCatElment.clear()
+        courseCatElment.send_keys(catnumber)
+        confirmElement.click()
+
+        yesButtonXpath="/html/body/form/div[1]/table/tbody/tr[4]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[6]/td[2]/input[1]"
+        yesButtonElement = wait.until(lambda driver: driver.find_element_by_xpath(yesButtonXpath))
+        yesButtonElement.click()
+
+        continueButtonXpath = "/html/body/form/div[1]/table/tbody/tr[4]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[7]/td[2]/span/input"
+        continueButtonElement= wait.until(lambda driver: driver.find_element_by_xpath(continueButtonXpath))
+        continueButtonElement.click()
+
     def tearDown(self):
-        self.driver.quit()
+        pass
+
+
+
 
 
 if __name__ == '__main__':
